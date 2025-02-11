ecc12c1a29ffd87140a5c455bc645be2e3fb29b0
huzeyfeyer <huzeyfeyer1@gmail.com>
2025-02-11 11:29:42 +0100

    m2 gedaan

---
+++ Module_0/M1/m1.js
@@ -0,0 +1,7 @@
+'use strict';
+function berekenen() {
+    let getal1 = parseInt(document.getElementById('getal1').value);
+    let getal2 = parseInt(document.getElementById('getal2').value);
+    let sum = getal1 + getal2;
+    alert(sum);
+}
